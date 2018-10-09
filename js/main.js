2c42ae98c0f38f5f9b2da0e596ad5cff62ef08cb
dave <dave.lee@uniquedonut.com>
2018-10-10 01:36:10 +0900

    navi 1depth imgMotion

--- js/main.js
+++ js/main.js
@@ -21,4 +21,14 @@ var flipTheBurger = new TimelineMax({paused:true, reversed:true})
 
 document.getElementById('sectionNav').onclick = function(e){
   flipTheBurger.reversed() ? flipTheBurger.play() : flipTheBurger.reverse();
+  rotateImgTween.play();
 }
+
+var rotateImg = document.getElementsByClassName('rotateImg');
+TweenMax.set(rotateImg, {autoAlpha:0});
+var rotateImgTween = new TimelineMax({paused:true,repeat:-1,reversed:true});
+for (var i = 0, len = rotateImg.length; i < len; i++) {
+  rotateImgTween.add(TweenMax.to(rotateImg[i], 1,{autoAlpha:1,rotation:360,transformOrigin:"50% 50%"},i));
+  rotateImgTween.add(TweenMax.to(rotateImg[i], 1,{autoAlpha:0,delay:2},i));
+}
+
